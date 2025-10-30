ac30e2c6841c4827fbb63bfae3df5478a9633951
roadtrip0108-oss <roadtrip0108@gmail.com>
2025-10-31 00:38:35 +0900

    09_3

--- 09.c
+++ 09.c
@@ -3,22 +3,24 @@
 #define SIZE 5
 
 int main(int argc, char *argv[])
+
 {
-  int i, average;
-  int sum;
-  int grade[SIZE];
+  int i;
+  int a[SIZE] = {1,2,3,4,5};
+  int b[SIZE] = {1,2,3,4,5};
+  int flag_same = 1;
   
-  sum=0;
-  for(i=0; i<SIZE; i++)
+  for(i=0;i<SIZE;i++)
   {
-           printf("학생 성적을 입력하세요 : ");
-           scanf("%d", &grade[i]);
-           sum += grade[i];
+         if(a[i] != b[i])
+         {
+           printf("%i-th element is diff.\n", i);
+           flag_same = 0;
+         } 
   }
   
-  average = sum / SIZE;
-  printf("성적평균 : %d\n", average); 
-      
+  printf("Are a and b the same? %i\n", flag_same);
+  
   system("PAUSE");	
   return 0;
 }

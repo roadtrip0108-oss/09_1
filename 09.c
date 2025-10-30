b6b336caa1038d78031d5cf0f6ab6ed01356a7c7
roadtrip0108-oss <roadtrip0108@gmail.com>
2025-10-31 01:01:21 +0900

    09_5

--- 09.c
+++ 09.c
@@ -1,37 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define SIZE 4
+#define ROWS 3
+#define COLS 3
 
-void square_array(int a[], int size);
-void print_array(int a[], int size);
-
-int main(int argc, char *argv[])
-
+void addMatrix(int A[][COLS], int B[][COLS], int C[][COLS])
 {
-  int list[SIZE] = {1,2,3,4};
-  
-  print_array(list, SIZE);
-  square_array(list, SIZE);
-  print_array(list, SIZE);
-  
-  system("PAUSE");	
-  return 0;
+     int i,j;
+     for (i=0;i<ROWS;i++)
+         for(j=0;j<COLS;j++)
+            C[i][j] = A[i][j] + B[i][j];
 }
 
-void square_array(int a[], int size)
+
+void printMatrix(int A[][COLS])
 {
-     int i;
+     int i, j;
+     for (i=0;i<ROWS;i++)
+     {
+         for(j=0;j<COLS;j++)
+            printf("%3d", A[i][j]);
+         printf("\n");
+     }
      
-     for(i=0;i<size;i++)
-        a[i] = a[i]*a[i];
 }
+         
 
-void print_array(int a[], int size)
+int main(void)
 {
-     int i;
-     
-     for(i=0;i<size;i++)
-        printf("%3d", a[i]);
-     printf("\n");
+    int A[ROWS][COLS] = { 
+        {2,3,0},
+        {8,9,1},
+        {7,0,5}};
+    int B[ROWS][COLS] = {
+        {1,0,0},
+        {0,1,0},
+        {0,0,1}};
+    int C[ROWS][COLS];
+    
+    addMatrix(A,B,C);
+    printMatrix(C);
+    
+    system("PAUSE");
+    return 0;
 }
-
